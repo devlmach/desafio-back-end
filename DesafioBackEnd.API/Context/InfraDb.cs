548d3e09d7a9d29564d5cf443465e92c44481418
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-11 14:51:00 -0300

    Criação e contextualização do banco de dados

---
+++ DesafioBackEnd.API/Context/InfraDb.cs
@@ -0,0 +1,23 @@
+﻿using DesafioBackEnd.API.Repository;
+using DesafioBackEnd.API.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioBackEnd.API.Context
+{
+    public static class InfraDb
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<IUsuarioService, UsuarioService>();
+
+            services.AddScoped<ITransacaoRepository, TransacaoRepository>();
+            services.AddScoped<ITransacaoService, TransacaoService>();
+
+            return services;
+        }
+    }
+}
