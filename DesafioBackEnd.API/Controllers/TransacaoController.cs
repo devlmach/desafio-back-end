32b3b523efce7af9e20ce563366b9be48a51fe88
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-11 15:08:04 -0300

    Criação da TransacaoController, método POST para criar transação

---
+++ DesafioBackEnd.API/Controllers/TransacaoController.cs
@@ -0,0 +1,31 @@
+﻿using DesafioBackEnd.API.Entity;
+using DesafioBackEnd.API.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioBackEnd.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransacaoController : ControllerBase
+    {
+        private readonly ITransacaoService _transacaoService;
+        public TransacaoController(ITransacaoService transacaoService)
+        {
+            _transacaoService = transacaoService;
+        }
+
+        [HttpPost]
+        [Route("/transfer")]
+        public async Task<ActionResult> CreateTransaction([FromBody] Transacao transacao)
+        {
+            if (transacao == null)
+                return BadRequest();
+
+            var result = await _transacaoService.CreateTransacaoAsync(transacao);
+
+            return Ok(result);
+        }
+
+
+    }
+}
