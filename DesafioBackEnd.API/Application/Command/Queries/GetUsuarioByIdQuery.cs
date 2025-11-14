bd436a4a0d0b954175d648d25efb35669caa89ef
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-14 16:09:32 -0300

    Criar query listar usuarios - todos e por id

---
+++ DesafioBackEnd.API/Application/Command/Queries/GetUsuarioByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using DesafioBackEnd.API.Domain.Entity;
+using MediatR;
+
+namespace DesafioBackEnd.API.Application.Command.Queries
+{
+    public class GetUsuarioByIdQuery : IRequest<Usuario>
+    {
+        public long Id { get; set; }
+
+        public GetUsuarioByIdQuery(long id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ DesafioBackEnd.API/Application/Command/Queries/GetUsuariosQuery.cs
@@ -0,0 +1,9 @@
+﻿using DesafioBackEnd.API.Domain.Entity;
+using MediatR;
+
+namespace DesafioBackEnd.API.Application.Command.Queries
+{
+    public class GetUsuariosQuery : IRequest<IEnumerable<Usuario>>
+    {
+    }
+}
