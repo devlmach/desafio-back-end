6f749916368cfd97ea1e61472726f5873d328b02
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-14 16:15:01 -0300

    Alterar estrtura do controler, incluindo retorno dos DTOs

--- DesafioBackEnd.API/Controllers/UsuarioController.cs
+++ DesafioBackEnd.API/Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
-﻿using DesafioBackEnd.API.Application.Service.Interfaces;
+﻿using DesafioBackEnd.API.Application.Dto.Usuario;
+using DesafioBackEnd.API.Application.Dto.Usuarios;
+using DesafioBackEnd.API.Application.Service.Interfaces;
 using DesafioBackEnd.API.Domain.Entity;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DesafioBackEnd.API.Controllers
@@ -15,8 +16,16 @@ namespace DesafioBackEnd.API.Controllers
             _usuarioService = usuarioService;
         }
 
+        /// <summary>
+        /// Endpoint para criar um usuário
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// <returns></returns>
         [HttpPost]
-        public async Task<ActionResult> CreateUsuario([FromBody] Usuario usuario)
+        [ProducesResponseType(typeof(CreateUsuarioDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<DetailUsuarioDto>> CreateUsuario([FromBody] CreateUsuarioDto usuario)
         {
             if (usuario == null)
                 return BadRequest();
@@ -25,8 +34,13 @@ namespace DesafioBackEnd.API.Controllers
             return Ok(usuario);
         }
 
+        /// <summary>
+        /// Endpoint para retornar um usuário pelo ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
-        public async Task<ActionResult> GetUsuarioById(int? id)
+        public async Task<ActionResult<DetailUsuarioDto>> GetUsuarioById(int? id)
         {
             var usuario = await _usuarioService.GetByIdAsync(id);
             if (usuario == null)
@@ -35,32 +49,50 @@ namespace DesafioBackEnd.API.Controllers
             return Ok(usuario);
         }
 
+        /// <summary>
+        /// Endpoint para retornar uma lista de usuários
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Usuario>>> GetAllUsuarios()
+        public async Task<ActionResult<IEnumerable<DetailUsuarioDto>>> GetAllUsuarios()
         {
             var usuarios = await _usuarioService.GetUsuariosAsync();
             return Ok(usuarios);
         }
 
+        /// <summary>
+        /// Endpoint para atualizar um usuário pelo ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="usuarioExists"></param>
+        /// <returns></returns>
         [HttpPut("{id}")]
-        public async Task<ActionResult> UpdateUsuario(int? id, [FromBody] Usuario usuarioExists)
+        public async Task<ActionResult> UpdateUsuario(int? id, [FromBody] UpdateUsuarioDto updateUsuarioDto)
         {
-            if (id != usuarioExists.Id || usuarioExists == null)
+            if (id != updateUsuarioDto.Id || updateUsuarioDto == null)
                 return BadRequest();
 
-            await _usuarioService.UpdateAsync(usuarioExists);
-            return Ok(usuarioExists);
+            await _usuarioService.UpdateAsync(updateUsuarioDto);
+            return Ok(updateUsuarioDto);
         }
 
-        [HttpDelete]
-        public async Task<ActionResult> DeleteUsuario(int? id)
+        /// <summary>
+        /// Endpoint para desativar um usuário
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(DetailUsuarioDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<DetailUsuarioDto>> DeleteUsuario(long? id)
         {
             var usuario = await _usuarioService.GetByIdAsync(id);
             if (usuario == null)
                 return NotFound();
 
+            usuario.IsActive = false;
             await _usuarioService.DeleteAsync(id);
-            return Ok("Deleted Successfully");
+            return Ok(usuario);
         }
     }
 }
