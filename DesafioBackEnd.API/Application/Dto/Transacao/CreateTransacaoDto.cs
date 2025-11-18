c6938e2357ccfa2d4370a8f3ad90766226d53250
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-18 12:17:15 -0300

    Classes Dto Transacao

--- DesafioBackEnd.API/Application/Dto/Transacao/CreateTransacaoDto.cs
+++ DesafioBackEnd.API/Application/Dto/Transacao/CreateTransacaoDto.cs
@@ -1,6 +1,16 @@
-﻿namespace DesafioBackEnd.API.Application.Dto.Transferencia
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioBackEnd.API.Application.Dto.Transacao
 {
     public class CreateTransacaoDto
     {
+        [Required(ErrorMessage = "O ID do pagador não pode ser vazio")]
+        public int? IdSender { get; set; }
+
+        [Required(ErrorMessage = "O ID do recebedor não pode ser vazio")]
+        public int? IdReceiver { get; set; }
+
+        [Required(ErrorMessage = "O valor a ser transferido não pode ser vazio")]
+        public decimal QuantiaTransferida { get; set; }
     }
 }
