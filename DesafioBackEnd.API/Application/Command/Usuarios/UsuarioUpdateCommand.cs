0a4cec5fcdad477fd46aacb771daea90b1354255
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-18 11:54:24 -0300

    Adicionar construtor classe

--- DesafioBackEnd.API/Application/Command/Usuarios/UsuarioUpdateCommand.cs
+++ DesafioBackEnd.API/Application/Command/Usuarios/UsuarioUpdateCommand.cs
@@ -3,5 +3,10 @@
     public class UsuarioUpdateCommand : UsuarioCommand
     {
         public long Id { get; set; }
+
+        public UsuarioUpdateCommand(long id)
+        {
+            Id = id;
+        }
     }
 }
