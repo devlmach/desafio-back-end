a23c47b5fe814c67e0a13955b8f6a77e4162a9cb
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-18 11:44:52 -0300

    Remover ProducesResponseType UsuarioController

--- DesafioBackEnd.API/Controllers/UsuarioController.cs
+++ DesafioBackEnd.API/Controllers/UsuarioController.cs
@@ -1,7 +1,5 @@
-﻿using DesafioBackEnd.API.Application.Dto.Usuario;
-using DesafioBackEnd.API.Application.Dto.Usuarios;
+﻿using DesafioBackEnd.API.Application.Dto.Usuarios;
 using DesafioBackEnd.API.Application.Service.Interfaces;
-using DesafioBackEnd.API.Domain.Entity;
 using DesafioBackEnd.API.Domain.Errors;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,13 +21,10 @@ namespace DesafioBackEnd.API.Controllers
         /// <param name="usuario"></param>
         /// <returns></returns>
         [HttpPost]
-        [ProducesResponseType(typeof(CreateUsuarioDto), StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<DetailUsuarioDto>> CreateUsuario([FromBody] CreateUsuarioDto usuario)
+        public async Task<ActionResult<CreateUsuarioDto>> CreateUsuario([FromBody] CreateUsuarioDto usuario)
         {
             if (usuario == null)
-                return BadRequest();
+                throw new BadRequestException("Invalid data.");
 
             await _usuarioService.AddAsync(usuario);
             return Ok(usuario);
@@ -54,6 +49,7 @@ namespace DesafioBackEnd.API.Controllers
         /// Endpoint para retornar uma lista de usuários
         /// </summary>
         /// <returns></returns>
+        /// <response code="200"> lista Obtida. </response>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<DetailUsuarioDto>>> GetAllUsuarios()
         {
@@ -65,13 +61,16 @@ namespace DesafioBackEnd.API.Controllers
         /// Endpoint para atualizar um usuário pelo ID
         /// </summary>
         /// <param name="id"></param>
-        /// <param name="usuarioExists"></param>
+        /// <param name="updateUsuarioDto"></param>
         /// <returns></returns>
         [HttpPut("{id}")]
         public async Task<ActionResult> UpdateUsuario(int? id, [FromBody] UpdateUsuarioDto updateUsuarioDto)
         {
-            if (id != updateUsuarioDto.Id || updateUsuarioDto == null)
-                return BadRequest();
+            if (id != updateUsuarioDto.Id)
+                throw new BadRequestException("Different id for operation.");
+
+            if (updateUsuarioDto == null)
+                throw new BadRequestException("Invalid data.");
 
             await _usuarioService.UpdateAsync(updateUsuarioDto);
             return Ok(updateUsuarioDto);
@@ -83,13 +82,11 @@ namespace DesafioBackEnd.API.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpDelete("{id}")]
-        [ProducesResponseType(typeof(DetailUsuarioDto), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<DetailUsuarioDto>> DeleteUsuario(long? id)
         {
             var usuario = await _usuarioService.GetByIdAsync(id);
             if (usuario == null)
-                return NotFound();
+                throw new NotFoundException($"User with id {id} not found.");
 
             usuario.IsActive = false;
             await _usuarioService.DeleteAsync(id);
