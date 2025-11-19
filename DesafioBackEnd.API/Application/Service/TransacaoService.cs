dd446b2bcf009affea4699d514c11c6d3daf5bd8
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-19 09:48:24 -0300

    Implemetação métodos das Transações

--- DesafioBackEnd.API/Application/Service/TransacaoService.cs
+++ DesafioBackEnd.API/Application/Service/TransacaoService.cs
@@ -1,29 +1,42 @@
-﻿using DesafioBackEnd.API.Application.Service.Interfaces;
-using DesafioBackEnd.API.Data.Repository.Interfaces;
-using DesafioBackEnd.API.Domain.Entity;
+﻿using AutoMapper;
+using DesafioBackEnd.API.Application.Command.Queries;
+using DesafioBackEnd.API.Application.Command.Transacoes;
+using DesafioBackEnd.API.Application.Dto.Transacao;
+using DesafioBackEnd.API.Application.Service.Interfaces;
+using MediatR;
 
 namespace DesafioBackEnd.API.Application.Service
 {
     public class TransacaoService : ITransacaoService
     {
-        private readonly ITransacaoRepository _transacaoRepository;
-        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
 
-        public TransacaoService(ITransacaoRepository transacaoRepository, IUsuarioRepository usuarioRepository)
+        public TransacaoService(IMapper mapper, IMediator mediator)
         {
-            _transacaoRepository = transacaoRepository;
-            _usuarioRepository = usuarioRepository;
+            _mapper = mapper;
+            _mediator = mediator;
         }
 
-        public async Task<Transacao> CreateTransacaoAsync(Transacao transacao)
+        public async Task CreateTransacaoAsync(CreateTransacaoDto createTransacaoDto)
         {
+            var transacaoCreateCommand = _mapper.Map<CreateTransacaoDto, TransacaoCreateCommand>(createTransacaoDto);
+            await _mediator.Send(transacaoCreateCommand);
+        }
 
-           // var idSender = await _usuarioRepository.GetByIdAsync(usuario.Id);
-            //var idReceiver = await _usuarioRepository.GetByIdAsync(usuario.Id);
+        public async Task<DetailTransacaoDto> GetTransacaoByIdAsync(long? id)
+        {
+            var transacao = new GetTransacaoByIdQuery(id.Value);
+            if (transacao == null)
+                throw new Exception($"Entity could not be found");
 
+            var result = await _mediator.Send(transacao);
+            return _mapper.Map<DetailTransacaoDto>(result);
+        }
 
-
-            return await _transacaoRepository.CreateTransacaoAsync(transacao);
+        public Task<IEnumerable<DetailTransacaoDto>> GetTransacoesAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
