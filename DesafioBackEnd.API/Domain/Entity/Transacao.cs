4a241081fe2163279f2981d11916e782986938a1
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-19 10:18:27 -0300

    Criação construtor

--- DesafioBackEnd.API/Domain/Entity/Transacao.cs
+++ DesafioBackEnd.API/Domain/Entity/Transacao.cs
@@ -5,17 +5,17 @@ namespace DesafioBackEnd.API.Domain.Entity
     public class Transacao : ClassBase
     {
         [Required(ErrorMessage = "O ID do pagador não pode ser vazio")]
-        public int? IdSender { get; set; }
+        public long? IdSender { get; set; }
 
         [Required(ErrorMessage = "O ID do recebedor não pode ser vazio")]
-        public int? IdReceiver { get; set; }
+        public long? IdReceiver { get; set; }
 
         [Required(ErrorMessage = "O valor a ser transferido não pode ser vazio")]
         public decimal QuantiaTransferida { get; set; }
 
         public Transacao() { }
 
-        public Transacao(int id, int idSender, int idReceiver, decimal quantiaTransferida, DateTime createdAt, DateTime updatedAt, bool isActive)
+        public Transacao(long id, long idSender, long idReceiver, decimal quantiaTransferida, DateTime createdAt, DateTime updatedAt, bool isActive)
         {
             Id = id;
             IdSender = idSender;
@@ -24,6 +24,14 @@ namespace DesafioBackEnd.API.Domain.Entity
             CreatedAt = createdAt;
             UpdatedAt = updatedAt;
             IsActive = isActive;
+        }
+
+        public Transacao(long? idSender, long? idReceiver, decimal quantiaTransferida, DateTime createdAt)
+        {
+            IdSender = idSender;
+            IdReceiver = idReceiver;
+            QuantiaTransferida = quantiaTransferida;
+            CreatedAt = createdAt;
         }
     }
 }
