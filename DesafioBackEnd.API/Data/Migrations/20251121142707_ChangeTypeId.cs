cc7d09ea4ec92cd6161c486f8390cca089b5b938
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-21 12:20:41 -0300

    Nova migration ChangeTypeId

---
+++ DesafioBackEnd.API/Data/Migrations/20251121142707_ChangeTypeId.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesafioBackEnd.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTypeId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "IdSender",
+                table: "Transacoes",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "IdReceiver",
+                table: "Transacoes",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IdSender",
+                table: "Transacoes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IdReceiver",
+                table: "Transacoes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
