e3b824bef115e4d7a8c0d1098cbb0203452f4ce9
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-24 11:06:46 -0300

    Criar TokenController

---
+++ DesafioBackEnd.API/Controllers/TokenController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioBackEnd.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+    }
+}
