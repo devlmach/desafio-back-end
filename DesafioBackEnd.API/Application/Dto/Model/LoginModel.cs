7b2e57c46c5adf2b4e592441f023360c4d73f934
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-11-24 11:04:06 -0300

    Criar DTO login e registro usuário

---
+++ DesafioBackEnd.API/Application/Dto/Model/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioBackEnd.API.Application.Dto.Model
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "invalid email format.")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+    }
+}
---
+++ DesafioBackEnd.API/Application/Dto/Model/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioBackEnd.API.Application.Dto.Model
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password don't match")]
+        public required string ConfirmPassword { get; set; }
+    }
+}
